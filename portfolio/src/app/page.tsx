9af308696882d162249dc84085e3bfd2a5f282a8
Jamal Mydeen <jamalmydeen112@gmail.com>
2024-05-31 21:46:25 +0530

    width fix

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -14,8 +14,8 @@ export default function Home() {
     <div className="ml-[100px] w-[calc(100%_-_100px)] flex base:flex-col bl:flex-row ">
       <div className="flex-1 flex flex-col px-14 py-[4.5rem] ">
         <div className="w-full flex flex-col gap-9">
-          <img src='/images/P2.png' alt="afdv" className="w-[92%] h-[230px] rounded-3xl" />
-          <div className="w-full flex flex-col gap-[13px] mt-1">
+          <img src='/images/P2.png' alt="afdv" className="w-[88%] h-[230px] rounded-3xl" />
+          <div className="w-[93%] flex flex-col gap-[13px] mt-1">
             <h2 className="text-xl font-[550] ">Hi, I&lsquo;m <span className="text-main">Jamal Mydeen</span></h2>
             <h1 className="text-5xl font-bold leading-[50px]">I develop <span className="relative text-[#16a34a]"><span className="z-[100000]">full-stack</span> <div className="absolute top-3 transform rotate-[-3deg] left-[-5px] z-[-10] bg-[#10292f]  w-full h-[90%] "></div></span> solutions that people <span className="text-pinky">adore.</span></h1>
             <p className="text-muted-foreground text-lg">Experienced Developer passionate about crafting innovative tech solutions . Outside of tech, I enjoy reading, writing, and exploring new destinations.</p>
